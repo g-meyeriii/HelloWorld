0c25ac4fc35409b84b648a8a0f91446564e5ec3b
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-04-30 10:25:56 -0700

    tried again

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myname = "George";
+
+            //string message = "My name is " + myname;
+
+            //string capsmessage = message.ToUpper();
+
+            //string lowermessage = message.ToLower();
+
+
+            //Console.WriteLine(lowermessage);
+
+            //Console.Read();
+
+            // implicit conversion
+            //int num = 1290532;
+            //long bignum = num;
+
+            //Console.WriteLine(bignum);
+            //Console.Read();
+
+            //float myFloat = 13.37f;
+            //double myNewDouble = myFloat;
+
+            //double myDouble = 13.37;
+            //int myInt;
+
+            //// explicit conversion
+            //// cast double to int;
+            //myInt = (int)myDouble;
+
+            //// typeconversion
+            //string myString = myDouble.ToString(); // "13.37"
+            //string myFloatString = myFloat.ToString();
+
+            //bool sunIsShining = true;
+
+            //string myBoolString =sunIsShining.ToString();
+
+            //string myString = "15";
+            //string mySecondString = "13";
+            //int num1 = Int32.Parse(myString);
+            //int num2 = Int32.Parse(mySecondString);
+            //int resultInt = num1 + num2;
+            //string result = myString + mySecondString;
+
+            Console.WriteLine();
+            Console.Read();
+        }
+
+    }
+}
