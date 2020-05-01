f4f68ed81ebf619eb1aa4c61f9b7ae6916861fb6
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-01 13:42:56 -0700

    If else high score problem

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,42 +6,75 @@ namespace HelloWorld
 {
     class Program
     {
-        static string username;
-        static string password;
+        
+        static int highscore = 300;
+        static string highScorePlayer= "George";
+        
 
         static void Main(string[] args)
         {
-            Register();
-            Login();
+            //int age = 17;
+
+            //if (age == 15)
+            //{
+            //    Console.WriteLine("Too young to skydive");
+            //}
+            //else if (age > 18)
+            //{
+            //    Console.WriteLine("Old enough to skydive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("How old are you then?");
+            //}
+
+            //switch (age)
+            //{
+            //    case 15:
+            //        Console.WriteLine("Too young to skydive");
+            //        break;
+            //    case 25:
+            //        Console.WriteLine("Old enough to skydive");
+            //        break;
+            //    default:
+            //        Console.WriteLine("How old are you then?");
+            //        break;
+            //}
+
+            //string username = "Root";
+
+            //switch (username)
+            //{
+            //    case "George":
+            //        Console.WriteLine("username is George");
+            //        break;
+            //    case "root":
+            //        Console.WriteLine("username is root");
+            //        break;
+            //    default:
+            //        Console.WriteLine("username is unknown");
+            //        break;
+            //}
+            CheckScore(295, "Joe");
+            CheckScore(310, "Luke");
+            CheckScore(350, "George");
+
             Console.Read();
         }
-        static void Register()
+        static void CheckScore(int score, string playerName)
         {
-            Console.WriteLine("Please enter username.");
-            username = Console.ReadLine();
-            Console.WriteLine("Please enter password");
-            password = Console.ReadLine();
-            Console.WriteLine("Registration complete!");
-        }
-        static void Login()
-        {
-            Console.WriteLine("Please enter username.");
-            if (username == Console.ReadLine())
+            if(highscore < score)
             {
-                Console.WriteLine("Please enter password");
-                if (password == Console.ReadLine())
-                {
-                    Console.WriteLine("Login complete!"); ;
-                }
-                else
-                {
-                    Console.WriteLine("Login failed, (password) restart program");
-                }
+                highscore = score;
+                highScorePlayer = playerName;
+                Console.WriteLine("New high score is "+ score);
+                Console.WriteLine("New highscore holder is " + playerName);
             }
             else
             {
-                Console.WriteLine("Login failed, (username) restart program");
+                Console.WriteLine("The old highscore of " + highscore + " is still held by " + highScorePlayer);
             }
         }
+       
     }
 }
